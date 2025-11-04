22261ccbb45cd56a15259ab971f426252adf9874
mehedi-imun <mehediimun@gmail.com>
2025-11-04 22:43:01 +0600

    style: simplify main element structure in DashboardLayout component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -16,9 +16,7 @@ export default function DashboardLayout({
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="flex-1 overflow-auto p-6 bg-gray-50">
-          {children}
-        </main>
+        <main className="flex-1 overflow-auto p-6 bg-gray-50">{children}</main>
       </div>
     </div>
   );
